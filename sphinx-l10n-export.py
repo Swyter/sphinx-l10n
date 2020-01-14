e80314277b13fcdeddf7ffe873fbf1316230baa9
Swyter <swyterzone@gmail.com>
2020-01-14 07:50:18 +0100

    Add a blacklist mechanism in the CSV -> JSON importer to get rid of the beta/technical strings that don't appear in the normal game. Get rid of the M_BOS_EFFECTS section, too.

--- sphinx-l10n-export.py
+++ sphinx-l10n-export.py
@@ -20,29 +20,54 @@ lang = {
     'MARKER_GREEK':      'el'
 }
 
-ignored_section_markers = [ 'M_STATIC_TEXT', 'M_BOS_EFFECTS' ]
-ignored_section_strings = [ 'HT_Text_Context_Blank', /* '', 'TO DO', 'DOES NOT APPEAR IN INVENTORY', 'TEST' */
-                            'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue20',
-                            'HT_Text_Inv_Dsc_BronzeAnkh',
-                            'HT_Text_Inv_Dsc_DummyItem',
-                            'HT_Text_Inv_Dsc_HD13_S02_Aby_Bourgeoisie_Racist',
-                            'HT_Text_Inv_Dsc_FL04_Skeletal_Bat_Fire',
-                            'HT_Text_Inv_Dsc_FL06_Undead_Man',
-                            'HT_Text_Inv_Dsc_KaDart',
-                            'HT_Text_ES_E_BOSItem',
-                            'HT_Text_ES_E_HelpText',
-                            'HT_Text_ES_E_BOSLocation',
-                            'HT_Text_ES_S_BOSItem',
-                            'HT_Text_ES_S_HelpText',
-                            'HT_Text_ES_S_BOSLocation',
-                            'HT_Text_Inv_Dsc_HD15_GuardTest',
-                            'HT_Text_Context_Test',
-                            'HT_Text_Notes_Test_D1',
-                            'HT_Text_Inv_Dsc_PossessionDart',
-                            'HT_Text_Inv_Dsc_PI06_Aka_Tentacle_Creature',
-                            'HT_Text_Inv_Dsc_PI06_Aka_Tentacle_Creature',
-                            'HT_Text_Inv_Dsc_PI06_Aka_Tentacle_Creature']
-
+ignored_languages = [ 'ja' ]
+ignored_section_markers = [ 'M_BOS_EFFECTS' ]
+ignored_section_strings = [
+    'HT_Text_Context_Blank', # '', 'TO DO', 'DOES NOT APPEAR IN INVENTORY', 'TEST'
+    'HT_Text_Context_Test',
+    'HT_Text_ES_E_BOSItem',
+    'HT_Text_ES_E_BOSLocation',
+    'HT_Text_ES_E_HelpText',
+    'HT_Text_ES_S_BOSItem',
+    'HT_Text_ES_S_BOSLocation',
+    'HT_Text_ES_S_HelpText',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue01',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue02',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue03',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue04',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue05',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue06',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue07',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue08',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue09',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue10',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue11',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue12',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue13',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue14',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue15',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue16',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue17',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue18',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue19',
+    'HT_Text_Hel_CursedPalace_FortuneTeller_AnkhPieceClue20',
+    'HT_Text_Inv_Dsc_BronzeAnkh',
+    'HT_Text_Inv_Dsc_DummyItem',
+    'HT_Text_Inv_Dsc_FL04_Skeletal_Bat',
+    'HT_Text_Inv_Dsc_FL04_Skeletal_Bat_Fire',
+    'HT_Text_Inv_Dsc_FL06_Undead_Man',
+    'HT_Text_Inv_Dsc_HD09_Horned_ReptileMan',
+    'HT_Text_Inv_Dsc_HD13_S02_Aby_Bourgeoisie_Racist',
+    'HT_Text_Inv_Dsc_HD15_GuardTest',
+    'HT_Text_Inv_Dsc_KaDart',
+    'HT_Text_Inv_Dsc_PI01_Green_Piranha',
+    'HT_Text_Inv_Dsc_PI05_Aka_Turtle',
+    'HT_Text_Inv_Dsc_PI06_Aka_Tentacle_Creature',
+    'HT_Text_Inv_Dsc_PL01_Eye',
+    'HT_Text_Inv_Dsc_PossessionDart',
+    'HT_Text_Inv_DummyItem',
+    'HT_Text_Memcard_Test'
+]
 
 data_read = []
 
@@ -73,6 +98,10 @@ for cur in lang:
     # swy: check that this spreadsheet actually contains a column for this language, it can happen
     if (cur not in data_read[HEADER_ROW]):
         print("warning: doesn't seem like this spreadsheet contains a %s; skipping it..." % cur)
+        continue
+        
+    # swy: some language columns are blank in the original translation; don't overwrite them
+    if cur in ignored_languages:
         continue
 
     # swy: get the column index for the current language
@@ -109,11 +138,15 @@ for cur in lang:
         # swy: change string M_SOMETHING sections and spew the previous one if it wasn't empty
         if row[0] != "" and cur_section != row[0]:
             print(">> pre_section", cur_section)
+            
+            # swy: blank out ignored sections altogether :)
+            if cur_section in ignored_section_markers:
+                print(" [s] ignoring section %s" % row[0])
+                out = collections.OrderedDict()
 
             if out: # swy: don't sort the files so that they appear in the correct section order, simplify the marker format instead
                 with open("%s/%s.json" % (lang[cur], cur_section.replace("M_", "").lower()), 'w') as outfile:
-                    if not row[0] in ignored_section_markers:
-                        json.dump(out, outfile, indent=2, ensure_ascii=False)
+                    json.dump(out, outfile, indent=2, ensure_ascii=False)
 
             cur_section_count = cur_section_count + 1
             cur_section = row[0];
@@ -134,8 +167,10 @@ for cur in lang:
         if row[MARKER_LANGUAGE_START_COLUMN + 1] and row[MARKER_LANGUAGE_START_COLUMN + 1] == "REMOVED":
             continue
 
-        # swy: ignore strings that are used but don't have translatable text:
-        if not row[MARKER_HASHCODE_COLUMN] in ignored_section_strings:
+        # swy: ignore beta/technical strings that are used but don't have/need translatable text:
+        if row[MARKER_HASHCODE_COLUMN] in ignored_section_strings:
+            print(" [i] ignoring %s" % row[MARKER_HASHCODE_COLUMN])
+            continue
 
         # swy: export it in this 'simple' format:
         #      https://docs.transifex.com/formats/chrome-json
